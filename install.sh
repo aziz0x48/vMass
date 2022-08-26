27f85512f727227cd663de89b4c8e5fb49613219
Aziz Hassine <azizc99tn@gmail.com>
2022-08-26 04:26:58 +0200

    version 1.2

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Installing Perl Modules, Please wait..";
+cpan install Net::IP
+cpan install Net::DNS::Resolver
+cpan install LWP::UserAgent
+cpan install HTTP::Request::Common
+cpan install WWW::Mechanize
+cpan install Term::ANSIColor
+echo "Perl Modules Installed Successfully. ✔";
